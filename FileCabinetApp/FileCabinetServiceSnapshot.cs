94ca53ded119b1c3635c325bcd2258a7d7ec8744
vladshalkevich@gmail.com <vladshalkevich@gmail.com>
2021-10-22 21:06:49 +0300

    Add export of file cabinet records to XML file.

---
+++ FileCabinetApp/FileCabinetServiceSnapshot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetServiceSnapshot
+    {
+        private readonly FileCabinetRecord[] records;
+        private readonly string fileTitle;
+
+        public FileCabinetServiceSnapshot(List<FileCabinetRecord> list, string title)
+        {
+            this.records = list?.ToArray();
+            this.fileTitle = title;
+        }
+
+        public void SaveToCSV(TextWriter writer)
+        {
+            FileCabinetRecordCsvWriter newWriter = new (writer);
+            newWriter.WriteTitle(this.fileTitle);
+            foreach (var record in this.records)
+            {
+                newWriter.Write(record);
+            }
+        }
+
+        public void SaveToXML(TextWriter writer)
+        {
+            FileCabinetRecordXmlWriter newWriter = new (writer);
+            newWriter.RootStart("records");
+            foreach (var record in this.records)
+            {
+                newWriter.Write(record);
+            }
+
+            newWriter.RootEnd();
+        }
+    }
+}
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 
 namespace FileCabinetApp
 {
@@ -13,6 +14,7 @@ namespace FileCabinetApp
         private const int CommandHelpIndex = 0;
         private const int DescriptionHelpIndex = 1;
         private const int ExplanationHelpIndex = 2;
+        private static readonly string[] AvailableExportFormats = { "csv", "xml" };
         private static readonly FileCabinetService Service = new ();
 
         private static readonly string[][] HelpMessages = new string[][]
@@ -33,6 +35,7 @@ namespace FileCabinetApp
             new Tuple<string, Action<string>>("list", List),
             new Tuple<string, Action<string>>("edit", Edit),
             new Tuple<string, Action<string>>("find", Find),
+            new Tuple<string, Action<string>>("export", Export),
         };
 
         private static BaseValidationRules validationRules;
@@ -237,6 +240,85 @@ namespace FileCabinetApp
             }
 
             Console.WriteLine();
+        }
+
+        private static void Export(string parameters)
+        {
+            string[] exportParams = parameters.Split(' ', 2);
+            var index = Array.FindIndex(AvailableExportFormats, 0, AvailableExportFormats.Length, match => match.Equals(exportParams[0], StringComparison.InvariantCultureIgnoreCase));
+            if (index < 0)
+            {
+                exportParams[1] = "Export format unsupported!";
+            }
+
+            var exportFormat = WriterFormatSwitch(index);
+
+            if (File.Exists(exportParams[1]))
+            {
+                ConsoleKey key;
+                do
+                {
+                    Console.WriteLine($"File is exist - rewrite {exportParams[1]} [Y / N]> ");
+                    key = Console.ReadKey().Key;
+                    Console.WriteLine();
+                }
+                while (!(((int)key) == 89 || (int)key == 78));
+
+                if (KeySwitch(key))
+                {
+                    exportFormat(exportParams[1]);
+                }
+            }
+            else
+            {
+                exportFormat(exportParams[1]);
+            }
+        }
+
+        private static Action<string> WriterFormatSwitch(int index) => index switch
+        {
+            0 => CallCSVWriter,
+            1 => CallXMLWriter,
+            _ => Console.WriteLine
+        };
+
+        private static bool KeySwitch(ConsoleKey key) => key switch
+        {
+            ConsoleKey.Y => true,
+            ConsoleKey.N => false,
+            _ => false
+        };
+
+        private static void CallCSVWriter(string parameters)
+        {
+            var newSnapshot = Service.MakeSnapshot();
+            try
+            {
+                StreamWriter writer = new (parameters, false, System.Text.Encoding.UTF8);
+                newSnapshot.SaveToCSV(writer);
+                writer.Close();
+                Console.WriteLine($"All records are exported to file {parameters}.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine($"Export failed: can't open file {parameters}");
+            }
+        }
+
+        private static void CallXMLWriter(string parameters)
+        {
+            var newSnapshot = Service.MakeSnapshot();
+            try
+            {
+                StreamWriter writer = new (parameters, false, System.Text.Encoding.UTF8);
+                newSnapshot.SaveToXML(writer);
+                writer.Close();
+                Console.WriteLine($"All records are exported to file {parameters}.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine($"Export failed: can't open file {parameters}");
+            }
         }
     }
 }
