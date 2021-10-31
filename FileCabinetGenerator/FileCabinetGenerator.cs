1b39eb976288d5fa30e650aed6a5c90384bfd6c8
vladshalkevich@gmail.com <vladshalkevich@gmail.com>
2021-10-31 14:28:31 +0300

    Add new app 'FileCabinetGenerator'

---
+++ FileCabinetGenerator/FileCabinetGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    class FileCabinetGenerator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
